a3342a47b7e31e063b5c4dacb09bab7fb3846a89
deguss <piri.daniel@gmail.com>
2015-08-20 23:08:41 +0000

    config file script ready

---
+++ conf/read_confs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+IFS=$'\n' read -d '' -r -a lines < files.txt
+
+for i in "${lines[@]}"; do
+	LF=$(echo $i | cut -c 2-)
+	echo -ne "$i -> $LF \t"
+	RES=$(cp --parents $i .)
+	if [ "$RES" == "" ]; then
+		echo '[copied]'
+	else
+		echo -e "\nERROR while copy!\n$RES\n\n"
+	fi
+done
+
+# crontabs
+crontab -l > ~/conf/crontab_pi
+sudo crontab -l > ~/conf/crontab_root
+
+
+
+
+
+
---
+++ conf/write_confs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+IFS=$'\n' read -d '' -r -a lines < files.txt
+
+#echo -e "${lines[@]}"
+
+for i in "${lines[@]}"; do
+	LF=$(echo $i | cut -c 2-)
+	echo -ne "$LF \t<-> $i \t"
+	DIFF=$(diff $LF $i)
+	if [ "$DIFF" == "" ]; then
+		echo '[IDENT]'
+	else
+		echo -e "\n$DIFF\n"
+		read -p "overwrite $i (y/n)? " choice
+		case "$choice" in 
+		  y|Y ) 
+				if [[ $EUID -ne 0 ]]; then
+  					echo -e "You must be root!"
+					exit 1
+				else
+					echo -e "\$ cp $LF $i :"
+		        	cp $LF $i
+				fi
+		      ;;
+		  * ) ;;
+		esac
+	fi
+done
+
+
+#
+#echo -e "${lines[1]}"
+#echo -e "${lines[2]}"
+
+
