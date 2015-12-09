2d3bb202d28578375f70c9c68a85236b55786fa1
developer-gps <info@opendatalogger.com>
2015-12-09 22:45:23 +0100

    watchdog tune, no sshd, syslog

--- scripts/plot_last_min.py
+++ scripts/plot_last_min.py
@@ -5,6 +5,8 @@ me = singleton.SingleInstance() # will sys.exit(-1) if other instance is running
 _LOCATION_IN = "/home/pi/data/tmp"
 _LOCATION_CHARTS = "/home/pi/www/charts"
 import numpy as np
+import matplotlib
+matplotlib.use('Agg') #no window should apper when drawing.
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import os, sys
@@ -16,20 +18,22 @@ t=np.zeros(1000*60,dtype='float32')
 def readfiles():
     import re, time
     from datetime import datetime as datetime
-    import netcdf    
+    import netcdf
+    import socket
+    
     global t, ch1, ch2, sps, sampl, title
     
     files = sorted(os.listdir(_LOCATION_IN))
     d=len(files)
     fn=int(files[-1][:-3]) #remove ending ".nc" and convert to seconds
     ts1=time.gmtime(fn)    #broken down time format
-    title=time.strftime("%F %T %z",ts1)
+    title=time.strftime("%F %T %z",ts1)+' on '+socket.gethostname()
     print "%d files found, most recent: %d.nc (%s)" % (len(files), fn, title)
     
     try:
         fid = netcdf.Dataset(os.path.join(_LOCATION_IN,files[-1]), 'r')
     except:
-        print "Unexpected error while reading file: %s \n %s" % (files[-1])
+        print "Unexpected error while reading file: %s" % (os.path.join(_LOCATION_IN,files[-1]))
         return -1
     else:
         start = getattr(fid,'start')      #some string
@@ -45,6 +49,7 @@ def readfiles():
         t=np.linspace(0,60,sampl)
         fid.close()
     print "file read"
+    return 1
         
 def plot_time():
     global t, ch1, ch2, sps, sampl, title
@@ -55,7 +60,7 @@ def plot_time():
     plt.plot(t, ch1, color='red')
     plt.ylabel('voltage (mV)')
     plt.grid(True)
-    plt.title('time plot from '+title+'' )    
+    plt.title('time plot from '+title)
     
     
     
@@ -83,13 +88,23 @@ def plot_psd():
     global t, ch1, ch2, sps, sampl, title
     
     plt.clf()
-    nfft=1024 #int(2**np.ceil(np.log2(sampl)));
+    nfft=2048 #int(2**np.ceil(np.log2(sampl)));
+
+    ax1=plt.subplot(211)
     (Pxx, freqs)=plt.psd(ch1, NFFT=nfft, Fs=sps, window=mlab.window_hanning)
+    plt.title('PSD (NFFT='+str(nfft)+', Fs='+str(sps)+'Hz, hann) from '+title)
+    plt.xlabel('')
+
+    ax2=plt.subplot(212)
     (Pxx, freqs)=plt.psd(ch2, NFFT=nfft, Fs=sps, window=mlab.window_hanning)
-    plt.legend(['ch1', 'ch2'], loc='upper right')
-    plt.title('Power Spectral Density (NFFT='+str(nfft)+', Fs='+str(sps)+', win=hann)')
-    
-    plt.savefig(os.path.join(_LOCATION_CHARTS,"last_psd.png"))
+    plt.ylabel('')
+    ax2.set_ylim(ax1.get_ylim())
+
+    fig=plt.gcf()
+    f_dpi=fig.get_dpi()    
+    fig.set_size_inches(1200.0/f_dpi, 500.0/f_dpi);
+
+    plt.savefig(os.path.join(_LOCATION_CHARTS,"last_psd.png"), dpi=96, bbox_inches='tight')
     plt.clf()
     print "plotting psd finished"
 
@@ -101,7 +116,7 @@ def plot_psd_resampled():
 
     ax1=plt.subplot(211)
     (Pxx, freqs)=plt.psd(ch1[::5], NFFT=nfft, Fs=sps/5, window=mlab.window_hanning, color='red')
-    plt.title('Power Spectral Density (NFFT='+str(nfft)+', Fs='+str(sps/5)+', win=hann)')
+    plt.title('PSD (NFFT='+str(nfft)+', Fs='+str(sps/5)+'Hz, hann) from '+title)
     plt.xlabel('')
 
     ax2=plt.subplot(212)
@@ -130,8 +145,9 @@ def plot_psd_resampled():
     
 
 if __name__ == "__main__":
-    readfiles()
+    if (readfiles() < 0):
+        exit(1)
     plot_time()
-    #plot_psd()
+    plot_psd()
     plot_psd_resampled()
     sys.stdout.flush()
